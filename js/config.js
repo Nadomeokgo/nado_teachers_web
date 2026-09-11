ad644672926a301ce06257177772b6e0ecaebcc7
nado3 <nado26327@gmail.com>
2026-09-11 11:26:03 +0900

    Bump schedule editor cache version

--- js/config.js
+++ js/config.js
@@ -29,7 +29,7 @@ window.NADO_CONFIG = {
 // Load the current schedule editor.
 (() => {
   const scheduleScript = document.createElement('script');
-  scheduleScript.src = 'js/schedule-v6.js?v=20260910-2';
+  scheduleScript.src = 'js/schedule-v6.js?v=20260911-1';
   scheduleScript.defer = true;
   document.head.appendChild(scheduleScript);
 
